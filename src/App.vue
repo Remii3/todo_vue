afc5cfc6e1c4841ea00f1d3dd70c4204244bf526
Remigiusz Woźniak <Remigiusz.Wozniak@forlagshuset.no>
2022-08-25 16:34:37 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,9 @@ const addTodo = () => {
     editable: false,
     createdAt: new Date().getTime(),
   });
+
+  input_content.value = '';
+  input_category.value = null;
 };
 
 const removeTodo = todo => {
